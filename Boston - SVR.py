9ef8642ca6345bb5148af6a0e80013ebbbbf372d
mbienvenu <42470122+mbienvenu@users.noreply.github.com>
2018-10-09 22:52:41 +1100

    Added SVR model and output

---
+++ Boston - SVR.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  9 21:41:10 2018
+
+@author: cecil
+"""
+
+
+
+#DAta processing
+
+import numpy as np
+import pandas as pd
+import csv
+
+
+
+boston_train = pd.read_csv("train.csv")
+boston_test = pd.read_csv("test.csv")
+
+
+X_train=boston_train.iloc[:,1:14].values 
+y_train=boston_train.iloc[:,14].values
+
+X_test = boston_test.iloc[:,1:14].values 
+
+#Fitting SVR to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X_train,y_train)
+
+
+#Building the predictions 
+y_pred = regressor.predict(X_test)
+
+
+with open("medv_submission_SVR.csv","w") as outfile:
+    writer = csv.writer(outfile,delimiter = "\n")
+    row = y_pred
+    writer.writerow(row)
+
+    
+outfile.close()
+
+
+
+
